9c870c50f38c76839147d76229d3750c3102db76
Rahul Dhakad <rahuldhakad201.rd@gmail.com>
2024-04-14 13:58:25 +0530

    add utils

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,9 @@
+// wrapper function
+
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).reject((err) => next(err));
+  };
+};
+
+export { asyncHandler };
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,49 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+import { ApiError } from "./ApiError";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    // upload file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    // delete file from server
+    fs.unlinkSync(localFilePath);
+
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+
+const deleteOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) {
+      throw new Error(
+        "Local file path is required for deleting a file from Cloudinary"
+      );
+    }
+
+    const deletionResponse = await cloudinary.uploader.destroy(localFilePath);
+
+    console.log("File deleted successfully from Cloudinary", deletionResponse);
+
+    return deletionResponse;
+  } catch (error) {
+    console.error("Error deleting file from Cloudinary:", error.message);
+    throw error;
+  }
+};
+
+export { uploadOnCloudinary, deleteOnCloudinary };
